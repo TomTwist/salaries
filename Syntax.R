d93d7c4ad5fe04269ddabfd02be59a42154173c3
Thomas Rudel <thomas.rudel@hotmail.com>
2019-08-16 22:25:18 +0200

    syntax hinzufügen

---
+++ Syntax.R
@@ -0,0 +1,6 @@
+# Aufrufen der Pakete
+library(readr)
+library(mosaic)
+
+# Einlesen und Benennen des Datensatzes
+salaries <- read_csv("http://vincentarelbundock.github.io/Rdatasets/csv/carData/Salaries.csv")
