fb16fdf0f43c4b2ef1ec1c0216b8b66577432180
Thomas Rudel <thomas.rudel@hotmail.com>
2019-08-17 19:19:45 +0200

    added names()

--- Syntax.R
+++ Syntax.R
@@ -6,5 +6,9 @@ library(dplyr)
 # Einlesen und Benennen des Datensatzes
 salaries <- read_csv("http://vincentarelbundock.github.io/Rdatasets/csv/carData/Salaries.csv")
 
+# Auswahl der benötigten Variablen. DF wird als neuer, reduzierter Datensatz angelegt
 DF <- salaries %>%
   select(rank, yrs.service, sex, salary)
+
+# Ausgabe der vorselektierten Variablen
+names(DF)
