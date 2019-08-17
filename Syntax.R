5f414cac2236440de4fedb7fcaee7641d6aa1f43
Thomas Rudel <thomas.rudel@hotmail.com>
2019-08-17 12:42:41 +0200

    created new DF with select filter to shorten the main Dataframe "salaries"

--- Syntax.R
+++ Syntax.R
@@ -1,7 +1,10 @@
 # Aufrufen der Pakete
 library(readr)
 library(mosaic)
+library(dplyr)
 
 # Einlesen und Benennen des Datensatzes
 salaries <- read_csv("http://vincentarelbundock.github.io/Rdatasets/csv/carData/Salaries.csv")
 
+DF <- salaries %>%
+  select(rank, yrs.service, sex, salary)
