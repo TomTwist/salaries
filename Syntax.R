c5d2883bf926f415a598b41204f961793d3402cc
Thomas Rudel <thomas.rudel@hotmail.com>
2019-08-17 19:46:38 +0200

    added summary()

--- Syntax.R
+++ Syntax.R
@@ -12,3 +12,10 @@ DF <- salaries %>%
 
 # Ausgabe der vorselektierten Variablen
 names(DF)
+
+# Statistiken inkl min und max von einzelnen Variblen
+favstats(~yrs.service, data = DF)
+favstats(~salary, data = DF)
+
+# summary liefert eine Übersicht über den Datensatz
+summary(DF)
